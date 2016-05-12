8bcab249afa58771a3e4eb3e7c453edbebaaa224
Will Losey <wlosey35@gmail.com>
2016-05-12 11:45:53 -0700

    Add files via upload

---
+++ sumcalc.py
@@ -0,0 +1,13 @@
+def summation(val):
+	sum = 0
+	for i in range (1,val+1):
+        sum = sum + i
+        
+    print(sum)
+      
+import sys
+val = int(sys.argv[1])
+print ("the summation of")
+print (val)
+print ("is")
+summation(val)
